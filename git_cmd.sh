9050711aa662c7ab705c1364ffe61774dd7e9f07
kazushi <kimoto@okayama-u.ac.jp>
2020-04-02 18:24:57 +0900

    Continue updating note1.tex

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,9 +1,11 @@
 git add git_cmd.sh
+
 git add 2020/Moodle/
 git add 2020/Lec1/*.tex
 git add 2020/Lec1/*.txt
 git add 2020/Lec1/*.svgz
 git add 2020/Lec1/makefile
+git add 2020/Lec1/Prb1
 
 git commit
 git push -u origin master
