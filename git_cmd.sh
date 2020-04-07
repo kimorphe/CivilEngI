3838eec26a4c27a589fbc524b47cac7c328a00da
kazushi <kimoto@okayama-u.ac.jp>
2020-04-07 17:49:55 +0900

    daily update

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,11 +1,11 @@
 git add git_cmd.sh
 
 git add 2020/Moodle/
-git add 2020/Lec1/*.tex
-git add 2020/Lec1/*.txt
-git add 2020/Lec1/*.svgz
-git add 2020/Lec1/makefile
-git add 2020/Lec1/Prb1
+git add 2020/Lec*/*.tex
+git add 2020/Lec*/*.txt
+git add 2020/Lec*/*.svgz
+git add 2020/Lec*/makefile
+git add 2020/Lec*/Prb1
 
 git commit
 git push -u origin master
